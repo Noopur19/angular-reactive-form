b1ba0afb0f9ec21231d28f1e551633866e9943fd
Noopur Chhajed <noopurchhajed98@gmail.com>
2019-07-29 22:33:30 +0530

    angular login done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormreactiveComponent } from './formreactive/formreactive.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormreactiveComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    ReactiveFormsModule 
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
